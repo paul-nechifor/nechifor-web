9f8e8fe4155b6d5af14093912713718d2df1ddd5
Paul Nechifor <paul@nechifor.net>
2017-07-08 23:52:32 +0100

    Add first actual test.

--- tests/buildMap.js
+++ tests/buildMap.js
@@ -5,9 +5,18 @@ const request = require('request');
 
 const root = process.env.site || 'http://localhost';
 
+const outerLinks = [];
+const innerLinks = ['/'];
+
+const innerLinksPrefix = [
+  '/',
+  root,
+];
+
+const seenLink = {'/': true};
+
 function loadPage(url, cb) {
   request(url, (err, res, html) => {
-    console.log('url', url)
     if (err) {
       return cb(err);
     }
@@ -15,13 +24,73 @@ function loadPage(url, cb) {
   });
 }
 
-function main(cb) {
-  loadPage(root, (err, $) => {
+function loadAllLinks(cb) {
+  let i = 0;
+
+  const load = () => {
+    processInnerLink(i, (err) => {
+      if (err) {
+        return cb(err);
+      }
+
+      if (++i < innerLinks.length) {
+        load();
+      } else {
+        cb();
+      }
+    });
+  };
+
+  load();
+}
+
+function processInnerLink(i, cb) {
+  const url = getInnerLink(i);
+  if (seenLink[url]) {
+    return cb();
+  }
+
+  loadPage(url, (err, $) => {
     if (err) {
       return cb(err);
     }
-    console.log($('head').html());
+
+    processPage(url, $);
+
+    $('a[href]').each(function () {
+      const href = $(this).attr('href');
+      if (seenLink[href]) {
+        return;
+      }
+      seenLink[href] = true;
+      if (_.some(innerLinksPrefix.map(x => href.indexOf(x) === 0))) {
+        if (href.indexOf('http://')) {
+          const url = href.substring(href.indexOf('/', 'http://'.length));
+          innerLinks.push(url || '/');
+        } else {
+          innerLinks.push(href);
+        }
+      } else {
+        outerLinks.push(href);
+      }
+    });
+
+    cb();
   });
+}
+
+function processPage(url, $) {
+  if (!$('head meta[charset="utf-8"]').length) {
+    console.log(url, 'missing <meta charset="utf-8">');
+  }
+}
+
+function getInnerLink(i) {
+  return root + innerLinks[i];
+}
+
+function main(cb) {
+  loadAllLinks(cb);
 }
 
 main((err) => {
